dbdd9e42b9ea9e357d2589b05c4a0d4973126653
Mehdi-133 <mehdikarbitou@gmail.com>
2025-12-30 16:16:29 +0100

    QP-3  Récupérer les catégories depuis la base de données

---
+++ classes/student_classes/StudentCategory.php
@@ -0,0 +1,28 @@
+<?php
+
+class StudentCategory
+{
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+   
+    public function getAll()
+    {
+        $sql = "SELECT c.*,
+                       COUNT(q.id) as quiz_count
+                FROM categories c
+                LEFT JOIN quiz q ON c.id = q.categorie_id
+                GROUP BY c.id
+                ORDER BY c.nom ASC";
+
+        $result = $this->db->query($sql);
+        return $result->fetchAll();
+    }
+}
+
+
