8e0da3fe7f63ba02cf9bd9383983729e0e154e16
Mehdi-133 <mehdikarbitou@gmail.com>
2026-01-02 15:46:46 +0100

    QP-26 Charger les questions du quiz

--- classes/student_classes/StudentQuestion.php
+++ classes/student_classes/StudentQuestion.php
@@ -1,1 +1,34 @@
+<?php
 
+
+class StudentQuestions
+{
+
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+
+    public function getAllQuestion($quizId)
+    {
+
+        $sql = "SELECT 
+        id,
+        quiz_id,
+        question,
+        option1,
+        option2,
+        option3,
+        option4
+        FROM questions
+        WHERE quiz_id = ?
+        ORDER BY id ASC;";
+
+        $result = $this->db->query($sql , [$quizId]);
+        return $result->fetchAll();
+    }
+}
--- classes/student_classes/StudentQuiz.php
+++ classes/student_classes/StudentQuiz.php
@@ -48,3 +48,5 @@ class StudentQuiz
         return $result->fetchAll();
     }
 }
+
+
